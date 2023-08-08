a77ab719474aeb6338c05e73c7e90c1fc87314e6
muradab <se.murad.abdella@gmail.com>
2023-08-09 00:06:25 +0300

    "completed week1 day 2 tasks"

---
+++ week1/day2/PalindromeChecker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+
+        Console.WriteLine("Enter your word:");
+        string input = Console.ReadLine();
+        string reversedString = new string(input.Reverse().ToArray());
+
+        if (input ==  reversedString){
+            Console.WriteLine("Word is palindrome");
+        }
+        else{
+            Console.WriteLine("Word is not palindrome");
+        }
+    
+    }
+}
---
+++ week1/day2/WordFrequencyCounter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+        Console.WriteLine("Enter your paragraph:");
+        string input = Console.ReadLine();
+        string[] words = input.Split(' ');
+
+        Console.WriteLine("Iterating over words:");
+        foreach (string word in words)
+        {
+            string cleanedWord = word.ToLower();
+
+            if (dictionary.ContainsKey(cleanedWord))
+            {
+                dictionary[cleanedWord]++;
+            }
+            else
+            {
+                dictionary[cleanedWord] = 1;
+            }
+        }
+
+        Console.WriteLine("Word frequencies:");
+        foreach (var kvp in dictionary)
+        {
+            Console.WriteLine($"Word: {kvp.Key}, Frequency: {kvp.Value}");
+        }
+    }
+}
